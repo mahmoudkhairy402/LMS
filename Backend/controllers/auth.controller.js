4b4f77e4cfae2da2f857b5634c136c12bdebb7b1
mahmoudkhairy402 <mahmoudkhairy402@gmail.com>
2026-04-24 06:55:05 +0300

    temp update to access coockies

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -66,8 +66,8 @@ function refreshCookieOptions() {
   const isProduction = process.env.NODE_ENV === "production";
   return {
     httpOnly: true,
-    secure: isProduction,
-    sameSite: isProduction ? "lax" : "none",
+    secure: false,
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   };
 }
